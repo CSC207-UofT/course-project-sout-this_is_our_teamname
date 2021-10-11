3788a5c6c04cb1b20f6652b7dfa6c67d501b0cc0
Liyu Feng(Liquid) <liyu.feng@mail.utoronto.ca>
2021-10-11 16:29:46 -0400

    TimeTableObj: Added decription and date as instances and getters for both of them

--- phase0/src/TimeTableObjects/TimeTableObject.java
+++ phase0/src/TimeTableObjects/TimeTableObject.java
@@ -5,16 +5,24 @@ package TimeTableObjects;
 
 public abstract class TimeTableObject {
     private String time;
+    private String date;
     private String location;
+    private String description;
 
     public TimeTableObject(Hashmap timeLocation) {
         this.time = time;
+        this.date = date;
         this.location = location;
+        this.description = '';
     }
 
     abstract void getTime();
 
+    abstract void getDate();
+
     abstract void getLocation();
+
+    abstract void getDescription();
 
     abstract void toString();
 
