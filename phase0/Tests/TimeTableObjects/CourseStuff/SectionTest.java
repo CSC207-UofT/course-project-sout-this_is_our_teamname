601946cddb515c1932e97e2e8f27f7b165bd6e54
Liyu Feng(Liquid) <liyu.feng@mail.utoronto.ca>
2021-10-15 20:32:18 -0400

    finished Timetable test docstring fdor StringToTime Unfinished SectionTest

---
+++ phase0/Tests/TimeTableObjects/CourseStuff/SectionTest.java
@@ -0,0 +1,37 @@
+package TimeTableObjects.CourseStuff;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SectionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void getCode() {
+    }
+
+    @Test
+    public void getProfessor() {
+    }
+
+    @Test
+    public void getFaculty() {
+    }
+
+    @Test
+    public void getDeliveryMethod() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+}
--- phase0/Tests/TimeTableStuff/TimeTableTest.java
+++ phase0/Tests/TimeTableStuff/TimeTableTest.java
@@ -20,45 +20,63 @@ class TimeTableTest {
     private TimeTableObject lecture3;
 
     // TODO double check on the format on Sections before running!
-    @BeforeEach
+    //@BeforeEach
     //public void setUp() {
-    //    LocalTime startTime1 = LocalTime.of(9,0,0);
-    //    LocalTime startTime2 = LocalTime.of(10, 0,0);
-    //    LocalTime endTime1 = LocalTime.of(10,0,0);
-    //    LocalTime endTime2 = LocalTime.of(11,0,0);
+    //    Time startTime1 = new Time(9,0,0);
+    //    Time startTime2 = new Time(10, 0,0);
+    //    Time endTime1 = new Time (10,0,0);
+    //    Time endTime2 = new Time(11,0,0);
 //
-    //    TimeTableObject lecture1 = new Section(startTime1,"2","SS100",Constants.MONDAY,Constants.YEAR,
+    //    TimeTableObject lecture1 = new Section(startTime1,endTime1,"SS100",Constants.MONDAY,Constants.YEAR,
     //            "MAT257","Gauss","Arts and Science","In Person");
-//
-    //    TimeTableObject lecture1 = new Section("1","2",Constants.MONDAY,"MY150","MAT257",
-    //            "Gauss","Arts and Science","Online");
-    //    TimeTableObject lecture2 = new Section("1","3",Constants.MONDAY,"MY151","MAT157",
-    //            "Descartes","Arts and Science","Online");
-    //    TimeTableObject lecture3 = new Section("1","3",Constants.FRIDAY,"MY151","MAT137",
-    //            "Alphonso","Arts and Science","Online");
+    //    TimeTableObject lecture2 = new Section(startTime1,endTime2,"SS101",Constants.MONDAY,Constants.FALL,
+    //            "MAT157", "Descartes","Arts and Science","Online");
+    //    TimeTableObject lecture3 = new Section(startTime2,endTime2,"SS100",Constants.MONDAY,Constants.FALL,
+    //            "MAT137", "Alphonso","Arts and Science","Online");
     //    TimeTable table = new TimeTable();
     //}
 
-    //TODO do we really need to test both schedule and checkConflicts? or am I not doing the test right?
     @Test
-//    public void schedule() {
-//        assertTrue(table.schedule(lecture1));
-//        assertFalse(table.schedule(lecture2));
-//        assertTrue(table.schedule(lecture3));
-//    }
+    public void schedule() {
+        Time startTime1 = new Time(9,0,0);
+        Time startTime2 = new Time(10, 0,0);
+        Time endTime1 = new Time (10,0,0);
+        Time endTime2 = new Time(11,0,0);
+
+        Section lecture1 = new Section(startTime1,endTime1,"SS100",Constants.MONDAY,Constants.YEAR,
+                "MAT257","Gauss","Arts and Science","In Person");
+        Section lecture2 = new Section(startTime1,endTime2,"SS101",Constants.MONDAY,Constants.FALL,
+                "MAT157", "Descartes","Arts and Science","Online");
+        Section lecture3 = new Section(startTime2,endTime2,"SS100",Constants.MONDAY,Constants.FALL,
+                "MAT137", "Alphonso","Arts and Science","Online");
+        TimeTable table = new TimeTable();
+        assertTrue(table.schedule(lecture1));
+        assertFalse(table.schedule(lecture2));
+        assertTrue(table.schedule(lecture3));
+    }
 
     //@Test
     //public void checkConflicts() {
+    //    Time startTime1 = new Time(9,0,0);
+    //    Time startTime2 = new Time(10, 0,0);
+    //    Time endTime1 = new Time (10,0,0);
+    //    Time endTime2 = new Time(11,0,0);
+//
+    //    Section lecture1 = new Section(startTime1,endTime1,"SS100",Constants.MONDAY,Constants.YEAR,
+    //            "MAT257","Gauss","Arts and Science","In Person");
+    //    Section lecture2 = new Section(startTime1,endTime2,"SS101",Constants.MONDAY,Constants.FALL,
+    //            "MAT157", "Descartes","Arts and Science","Online");
+    //    TimeTable table = new TimeTable();
     //    assertTrue(table.checkConflicts(lecture1));
     //    assertFalse(table.checkConflicts(lecture2));
     //}
-
-    @Test
-    public void testToString() {
+//
+    //@Test
+    //public void testToString() {
         //toString's hashmap prints the key in different order each time, so I am not sure how to test it.
         //Thought about testing for empty string, but that won't work since the keys are already there to be printed.
         //assertEquals(table.toString(), "");
 
         //maybe check for specific strings after slicing?
-    }
+    //}
 }
