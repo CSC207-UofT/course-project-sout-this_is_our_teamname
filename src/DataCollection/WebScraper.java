d6c321234e652660b7a16c8a9c824a24f40791e3
Sonny <1076659118@qq.com>
2021-11-07 22:17:17 -0500

    Some basic scraping for webscraper

--- src/DataCollection/WebScraper.java
+++ src/DataCollection/WebScraper.java
@@ -24,8 +24,31 @@ class Main {
             Document doc = Jsoup.connect(
                     "https://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&viewId=CourseDetails-InquiryView&courseId=CSC207H1F20219").get();
             System.out.println(doc.title());
-            Element ele = doc.getElementById("u272_line3");
-            System.out.println(ele);
+            // find element by combination of elements with id.
+            while
+            Elements ele = doc.select("span#u245_line2");
+            String code = ele.text();
+            // remove css tags.
+            code = code.replaceAll("(?is)<style.*?>.*?</style>", "");
+            System.out.println(code);
+
+            Elements ele2 = doc.select("span#u254_line2");
+            String time = ele2.text();
+            // remove css tags.
+            time = time.replaceAll("(?is)<style.*?>.*?</style>", "");
+            System.out.println(time);
+
+            Elements ele3 = doc.select("span#u263_line2");
+            String prof = ele3.text();
+            // remove css tags.
+            prof = prof.replaceAll("(?is)<style.*?>.*?</style>", "");
+            System.out.println(prof);
+
+            Elements ele4 = doc.select("span#u272_line2");
+            String loc = ele4.text();
+            // remove css tags.
+            loc = loc.replaceAll("(?is)<style.*?>.*?</style>", "");
+            System.out.println(loc);
 
 //            String description = doc.select("meta[name=description]").get(0).attr("content");
 //            System.out.println("Meta description : " + description);
