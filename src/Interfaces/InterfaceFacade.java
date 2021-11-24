8757b7ccc1ac7482e352a9633dcff51b59c96e17
Caules Ge <caules.ge@mail.utoronto.ca>
2021-11-24 03:13:41 -0500

    InterfaceFacade has been fixed

--- src/Interfaces/InterfaceFacade.java
+++ src/Interfaces/InterfaceFacade.java
@@ -1,8 +1,21 @@
 package Interfaces;
 
 import java.io.IOException;
-import java.util.Scanner;
 
+import java.util.Arrays;
+
+import Helpers.InputCheckers.InputChecker;
+import Helpers.InputCheckers.Predicate;
+
+
+/**
+ * A InterfaceFacade class that serves as the Invoker in the facade
+ * pattern. It sets the UserInterface and OperatorInterface and executes them according to input.
+ *
+ * === Private Attributes ===
+ *  userInterface: The UserInterface object.
+ *  operatorInterface: The OperatorInterface object.
+ */
 public class InterfaceFacade {
     private final UserInterface userInterface;
     private final OperatorInterface operatorInterface;
@@ -14,35 +27,37 @@ public class InterfaceFacade {
 
     /**
      * Runs the InterfaceFacade.
+     * The IOException will be raised if the given file is not found.
      */
     public void run() throws IOException {
         // As long as the program is running
         boolean running = true;
-
         while (running) {
-            System.out.println("Who are you (User/Operator): ");
-            Scanner objectScanner = new Scanner(System.in);
-            String type = objectScanner.nextLine();
-            if (type.equals("User")) {
+            // Check who is running the program: User or Operator.
+            InputChecker requestRole = new InputChecker("Who are you (User/Operator): ",
+                new isValidRole());
+            String role = requestRole.checkCorrectness();
+            if (role.equals("User")) {
                 this.userInterface.run();
-            } else if (type.equals("Operator")) {
+            } else if (role.equals("Operator")) {
                 this.operatorInterface.run();
-            } else {
-                System.out.print("Please type (User/Operator).");
             }
+
 
             // Check if the user want to exit the program.
-            Scanner continueQuestion = new Scanner(System.in);
-            System.out.println("Do you want to exit? " +
-                    "(true/false):");
-            String continueResponse = continueQuestion.nextLine();
+            InputChecker requestContinue = new InputChecker("Do you want to exit? " +
+                    "(true/false):",
+                    new isValidContinue());
+
+            String whetherContinue = requestContinue.checkCorrectness();
 
             // Checks if the user wants to add any more courses.
-            if (continueResponse.equals("true")){
-                running = false;
+            if (whetherContinue.equals("true")){
+                running = false;}
+
             }
         }
-    }
+
 
     /**
      * A InterfaceFacade. The main method of the program and the one that the
@@ -56,7 +71,33 @@ public class InterfaceFacade {
         InterfaceFacade facade = new InterfaceFacade(user, user.getOperator());
 
         facade.run();
-
     }
 
+
+
+    // ============================ Predicates =================================
+    private static class isValidRole extends Predicate {
+        private final String[] allowed;
+        public isValidRole(){
+            this.allowed = new String[]{"User", "Operator"};
+        }
+
+        @Override
+        public boolean run(String prompt) {
+            return Arrays.asList(allowed).contains(prompt) ;
+        }
+    }
+
+
+    private static class isValidContinue extends Predicate {
+        private final String[] allowed;
+        public isValidContinue(){
+            this.allowed = new String[]{"true", "false"};
+        }
+
+        @Override
+        public boolean run(String prompt) {
+            return Arrays.asList(allowed).contains(prompt) ;
+        }
+    }
 }
