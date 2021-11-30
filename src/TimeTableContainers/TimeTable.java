0c1e3aaa59e74df770a110ca2dc0efc038c25fa9
matthewrhdu <matthew.du@mail.utoronto.ca>
2021-11-30 13:25:59 -0500

    Fixed and rearranged classes

--- src/TimeTableContainers/TimeTable.java
+++ src/TimeTableContainers/TimeTable.java
@@ -14,53 +14,27 @@ import java.util.LinkedHashMap;
 /**
  * TimeTable class stores all the activities from Monday to Sunday. If there is a conflict when storing a new activity,
  * it will still be stored, and a conflict warning will be sent back prompting user to take action or ignore it.
+ *
  * === Private Attributes ===
- * calendar is a single timetable from Monday to Friday, and 24 intervals per day that can be filled with an event.
- * tasks contain Task objects(as values) in the corresponding weekday(as keys)
+ * calendar: a single timetable from Monday to Friday, and 24 intervals per
+ *  day that can be filled with an event.
+ * tasksCalender: contain Task objects(as values) in the corresponding weekday
+ *  (as keys)
  */
 public class TimeTable {
     private final LinkedHashMap<String, Events[]> calendar;
     private final LinkedHashMap<String, ArrayList<Task>> taskCalendar;
 
     public TimeTable() {
-        this.calendar = new LinkedHashMap<>() {{
-            put(Constants.MONDAY, new Events[24]);
-            put(Constants.TUESDAY, new Events[24]);
-            put(Constants.WEDNESDAY, new Events[24]);
-            put(Constants.THURSDAY, new Events[24]);
-            put(Constants.FRIDAY, new Events[24]);
-            put(Constants.SATURDAY, new Events[24]);
-            put(Constants.SUNDAY, new Events[24]);
-        }};
-        this.taskCalendar = new LinkedHashMap<>() {{
-            put(Constants.MONDAY, new ArrayList<>());
-            put(Constants.TUESDAY, new ArrayList<>());
-            put(Constants.WEDNESDAY, new ArrayList<>());
-            put(Constants.THURSDAY, new ArrayList<>());
-            put(Constants.FRIDAY, new ArrayList<>());
-            put(Constants.SATURDAY, new ArrayList<>());
-            put(Constants.SUNDAY, new ArrayList<>());
-        }};
-
+        this.calendar = new LinkedHashMap<>(){};
+        this.taskCalendar = new LinkedHashMap<>(){};
+        for (String day : Constants.DAYS_OF_THE_WEEK){
+            this.calendar.put(day, new Events[24]);
+            this.taskCalendar.put(day, new ArrayList<>());
+        };
     }
 
-    /**
-     * Returns a copy of the timetable (not alias)
-     *
-     * @return the copy of the timetable
-     */
-    public LinkedHashMap<String, Events[]> getCopy() {
-        LinkedHashMap<String, Events[]> copy = new LinkedHashMap<>();
-        for (String day : this.calendar.keySet()) {
-            Events[] events = new Events[24];
-            for (int i = 0; i < 24; i++) {
-                events[i] = this.calendar.get(day)[i];
-            }
-            copy.put(day, events);
-        }
-        return copy;
-    }
-
+    // ========================= Basic Operations ==============================
     /**
      * Schedules the given activity into the appropriate weekday.
      *
@@ -74,7 +48,7 @@ public class TimeTable {
             int start = event.getStartTime().getHour();
             int end = event.getEndTime().getHour();
 
-            //Add activity to interval between startTime and endTime
+            // Add activity to interval between startTime and endTime
             for (int i = start; i < end; i++) {
                 this.calendar.get(event.getDate())[i] = event;
             }
@@ -90,15 +64,26 @@ public class TimeTable {
      * @return true if there is no conflict, false otherwise
      */
     public boolean checkConflicts(Events activity) {
-        Events[] weekday = calendar.get(activity.getDate());
+        // Get all events on that day of the week
+        Events[] activitiesOnDay = calendar.get(activity.getDate());
+
         int start = activity.getStartTime().getHour();
         int end = activity.getEndTime().getHour();
+
         for (int i = start; i < end; i++) {
-            if (weekday[i] != null) {
+            if (activitiesOnDay[i] != null) {
                 return false;
             }
         }
         return true;
+    }
+
+    /**
+     * Adds a Task object to the taskCalendar
+     * @param task is a Task object
+     */
+    public void addTasks(Task task) {
+        this.taskCalendar.get(task.getDate()).add(task);
     }
 
     /**
@@ -124,8 +109,74 @@ public class TimeTable {
     }
 
     /**
-     * Check if the given course is present in this TimeTable
+     * Generate the String representation of the calender.
+     * @return the string of calendar
+     */
+    public String toString() {
+
+        StringBuilder timeStrings = new StringBuilder();
+
+        // For each day
+        for (String day : this.calendar.keySet()) {
+
+            // Add the day header
+            StringBuilder times = new StringBuilder(day + ":\n");
+
+            Events[] events = this.calendar.get(day);
+            for (int i = 0; i < events.length; i++) {
+
+                // Append the time as a string
+                times.append("\t").append(i).append(":00 ");
+
+                // Append the event as a string at that time
+                if (events[i] != null) {
+                    times.append(events[i]).append("\n");
+                } else {
+                    times.append("\n");
+                }
+            }
+            timeStrings.append(times);
+        }
+        return timeStrings.toString();
+    }
+
+    // ===================== Setters and Getters ===============================
+    /**
+     * Returns a copy of the timetable (not alias)
      *
+     * @return the copy of the timetable
+     */
+    public LinkedHashMap<String, Events[]> getCopy() {
+        LinkedHashMap<String, Events[]> copy = new LinkedHashMap<>();
+        for (String day : this.calendar.keySet()) {
+            Events[] events = new Events[24];
+            for (int i = 0; i < 24; i++) {
+                events[i] = this.calendar.get(day)[i];
+            }
+            copy.put(day, events);
+        }
+        return copy;
+    }
+
+    /**
+     * Get the task hashmap
+     * @return the task hashmap
+     */
+    public LinkedHashMap<String, ArrayList<Task>> getTaskCalendar() {
+        return taskCalendar;
+    }
+
+    /**
+     * Get the calender of the timetable
+     * @return the calender contained in the timetable
+     */
+    public LinkedHashMap<String, Events[]> getCalendar() {
+        return this.calendar;
+    }
+
+    /**
+     * Check if the given course is present in this TimeTable
+     * TODO This method is never used, and incorrect
      * @param course The course to be checked
      * @return true if the course is present, false otherwise
      */
@@ -143,51 +194,6 @@ public class TimeTable {
             }
         }
         return false;
-    }
-
-    /**
-     * Adds a Task object to the taskCalendar
-     * @param task is a Task object
-     */
-    public void addTasks(Task task) {
-        this.taskCalendar.get(task.getDate()).add(task);
-    }
-
-    /**
-     * Get the task hashmap
-     * @return the task hashmap
-     */
-    public LinkedHashMap<String, ArrayList<Task>> getTaskCalendar() {return taskCalendar;}
-
-    /**
-     * Get the calender of the timetable
-     * @return the calender contained in the timetable
-     */
-    public LinkedHashMap<String, Events[]> getCalendar() {
-        return this.calendar;
-    }
-
-    /**
-     * Generate the String representation of the calender.
-     * @return the string of calendar
-     */
-    public String toString() {
-        StringBuilder timeStrings = new StringBuilder();
-        for (String day : this.calendar.keySet()) {
-            StringBuilder times = new StringBuilder(day + ":\n");
-
-            Events[] events = this.calendar.get(day);
-            for (int i = 0; i < events.length; i++) {
-                times.append("\t").append(i).append(":00 ");
-                if (events[i] != null) {
-                    times.append(events[i]).append("\n");
-                } else {
-                    times.append("\n");
-                }
-            }
-            timeStrings.append(times);
-        }
-        return timeStrings.toString();
     }
 }
 
