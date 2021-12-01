b55e7c7f06afd4d19d60b7916e4490e28408a09e
Aiden <aiden.lin@mail.utoronto.ca>
2021-12-01 16:35:26 -0500

    Change in DataLoading

--- src/DataLoading/CSVDownloader.java
+++ src/DataLoading/CSVDownloader.java
@@ -1,15 +1,13 @@
 package DataLoading;
 
-import TimeTableObjects.EventObjects.Task;
 import Helpers.Constants;
-// TODO. In short, I don't think timetable should appear here at all. If
-//  timetable is here, there is a problem with the dependency still!
 import TimeTableContainers.TimeTable;
 import TimeTableContainers.TimeTableManager;
+import TimeTableObjects.EventObjects.Task;
 import TimeTableObjects.Events;
 
-
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +15,7 @@ import java.util.List;
  * A class that downloads a timetable manager object to a properly formatted csv file
  *
  */
-public class CSVDownloader extends DataLoader {
+public class CSVDownloader{
 
     /**
      * Download a timetable manager object to a properly formatted csv file
@@ -27,26 +25,19 @@ public class CSVDownloader extends DataLoader {
      * @param filename the name that users want to save the file as
      * @param year the year of the timetables that users input
      */
-    @Override
     public void download(TimeTableManager theManager, String filename, String year) throws IOException {
         for (String term : theManager.getTerms()) {
 
-            // TODO Merge the two lines
-            TimeTable timetable = theManager.getTimetable(term);
-            List<List<String>> dataLists = TimetableToList(timetable);
+            List<List<String>> dataLists = TimetableToList(theManager.getTimetable(term));
 
             FileWriter csvWriter = new FileWriter("src\\OutputFiles\\"
                     + filename + "_" + year + "_"  + term + ".csv");
 
             StringBuilder heading = new StringBuilder();
-            // TODO Use a for Loop on these or instantiate them directly. No
-            //  need for repeated statements like this
-            heading.append("Date").append(",");
-            heading.append("Time").append(",");
-            heading.append("Event Type").append(",");
-            heading.append("Event Name").append(",");
-            heading.append("Section Code").append(",");
-            heading.append("Description").append(",");
+            String[] headingString = {"Date", "Time", "Event Type", "Event Name", "Section Code", "Description"};
+            for (String word : headingString) {
+                heading.append(word).append(",");
+            }
 
             csvWriter.append(heading.append("\n"));
 
@@ -63,39 +54,62 @@ public class CSVDownloader extends DataLoader {
     /**
      * Convert a timetable to a list of lists of strings that contains the data
      *
-     * TODO This method is too long. Break it off into helper methods! Also
-     * TODO add comments and refactor the names to help readability!
-     * TODO (i.e. dataLists and dataList. That's just confusing. I think it
-     * TODO would be good to name them better) See naming conventions in
-     * TODO JavaAndExceptions.pdf in Week 4.
-     *
      * @param timetable the timetable that needs to be converted
      */
     private List<List<String>> TimetableToList(TimeTable timetable) {
-        List<List<String>> dataLists = new ArrayList<>();
+        List<List<String>> eventLists = new ArrayList<>();
 
+        // Search all days in a week and get data from both the calendar and the task calendar
         for (String day : Constants.DAYS_OF_THE_WEEK) {
+            // Search every hour period in a day and get data from the calendar
             for (int n = 0; n <= 23; n ++) {
+                // If there are non-empty data during this hour
                 if (timetable.getCalendar().get(day)[n] != null) {
-                    List<String> datalist = new ArrayList<>();
-                    datalist.add(0, day);
-                    Events item = timetable.getCalendar().get(day)[n];
-                    datalist.add(1, item.getStartTime().toString() +
-                            "-" + item.getEndTime().toString());
-                    datalist.addAll(timetable.getCalendar().get(day)[n].reconstruct());
-                    dataLists.add(datalist);
+                    CalendarHelper(eventLists, day, timetable.getCalendar().get(day)[n]);
                 }
             }
+            // If there are non-empty data in the task calendar
             if (!(timetable.getTaskCalendar().get(day).isEmpty())) {
-                List<String> taskList = new ArrayList<>();
-                taskList.add(day);
-                taskList.add("Tasks");
-                for (Task task : timetable.getTaskCalendar().get(day)) {
-                    taskList.addAll(task.reconstruct());
-                }
-                dataLists.add(taskList);
+                TaskCalendarHelper(eventLists, day, timetable.getTaskCalendar().get(day));
             }
         }
-        return dataLists;
+        return eventLists;
+    }
+
+    /**
+     * Append one more list of data to the list of lists of strings
+     *
+     * @param eventLists the list of lists of strings that needs to be enlarged
+     * @param day the day information of the new list of data
+     * @param item the event information of the new list of data
+     */
+    private void CalendarHelper(List<List<String>> eventLists, String day, Events item) {
+        // Format one line
+        List<String> eventList = new ArrayList<>();
+        eventList.add(0, day);
+        eventList.add(1, item.getStartTime().toString() +
+                "-" + item.getEndTime().toString());
+        eventList.addAll(item.reconstruct());
+        // Append the line
+        eventLists.add(eventList);
+    }
+
+    /**
+     * Append one more list of data to the list of lists of strings
+     *
+     * @param eventLists the list of lists of strings that needs to be enlarged
+     * @param day the day information of the new list of data
+     * @param item the event information of the new list of data
+     */
+    private void TaskCalendarHelper(List<List<String>> eventLists, String day, ArrayList<Task> tasks) {
+        // Format one line
+        List<String> taskList = new ArrayList<>();
+        taskList.add(day);
+        taskList.add("Tasks");
+        for (Task task : tasks) {
+            taskList.addAll(task.reconstruct());
+        }
+        // Append the line
+        eventLists.add(taskList);
     }
 }
