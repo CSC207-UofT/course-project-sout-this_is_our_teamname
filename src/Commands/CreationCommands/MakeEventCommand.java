a20125756edfa496e9753d5593c73dfb0477be93
matthewrhdu <matthew.du@mail.utoronto.ca>
2021-12-02 00:19:08 -0500

    Fixed Activity

--- src/Commands/CreationCommands/MakeEventCommand.java
+++ src/Commands/CreationCommands/MakeEventCommand.java
@@ -1,7 +1,6 @@
 package Commands.CreationCommands;
 
 import Commands.Command;
-import TimeTableObjects.EventFactory;
 import TimeTableObjects.EventObjects.Activity;
 import TimeTableObjects.Events;
 import TimeTableObjects.EventObjects.Task;
@@ -15,7 +14,6 @@ import TimeTableContainers.TimeTableManager;
 import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
@@ -58,7 +56,7 @@ public class MakeEventCommand implements Command {
         while (running) {
             HashMap<String, String> responses = promptUser();
 
-            Events toSchedule = EventFactory.getCorrectTimeTableObject(
+            Events toSchedule = getCorrectTimeTableObject(
                     StringToTime.makeTime(responses.get(START_TIME)),
                     StringToTime.makeTime(responses.get(END_TIME)),
                     responses.get(LOCATION),
@@ -128,6 +126,38 @@ public class MakeEventCommand implements Command {
      */
     protected boolean hasScheduled(){
         return scheduledObject != null;
+    }
+
+    /**
+     * A helper method for scheduling events.
+     *
+     * @param startTime the start time
+     * @param endTime the end time
+     * @param theLocation the location
+     * @param theDate the date
+     * @param term the term
+     * @param type the type of object
+     * @return event "cast" to the correct type.
+     */
+    public static Events getCorrectTimeTableObject(LocalTime startTime,
+                                                   LocalTime endTime,
+                                                   String theLocation,
+                                                   String theDate,
+                                                   String term,
+                                                   String type) {
+        // Creates the Activity
+        switch (type){
+            case Constants.ACTIVITY:
+                return new Activity(startTime, endTime, theDate, term , "");
+            case Constants.TASK:
+                Task task = new Task(startTime, endTime, theDate, term);
+                task.addToName(theLocation);
+                return task;
+            // ...
+            // Add more types of events here!
+            default:
+                return null;
+        }
     }
 
     // ====================== Predicates =======================================
