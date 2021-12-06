d25d02a050cfdd739c07a62c1d75350282336f60
Liyu Feng(Liquid) <liyu.feng@mail.utoronto.ca>
2021-12-05 23:14:23 -0500

    fixed duplicate scheduling for activity in Jtab. defaultTableModel is the cause

--- src/demoGUI/userview/TimeTableScreen.java
+++ src/demoGUI/userview/TimeTableScreen.java
@@ -34,7 +34,6 @@ public class TimeTableScreen extends JFrame {
     private JLabel titleLable;
     private JTabbedPane timeTableTabs;
     private JPanel UnderTabsPanel;
-    private DefaultTableModel defaultTableModel;
     private JScrollPane TimeTableScrollPane;
     private JComboBox<String> comboBox1;
     private JComboBox<String> comboBox2;
@@ -60,9 +59,6 @@ public class TimeTableScreen extends JFrame {
         loadButton.addActionListener(timeTableScreenController);
         settingsButton.addActionListener(timeTableScreenController);
 
-        //set new tableModel
-        defaultTableModel = new DefaultTableModel(25, 8);
-
         // Terminate program
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -72,6 +68,7 @@ public class TimeTableScreen extends JFrame {
 
     public void refreshTimetableTabs(TimeTableManager manager) {
         timeTableTabs.removeAll();
+        //cycle through the timetables in manager using the name of the timetables
         for (String term : manager.getTerms()) {
             JPanel panel = new JPanel();
 
@@ -81,20 +78,13 @@ public class TimeTableScreen extends JFrame {
             table.setPreferredScrollableViewportSize(table.getPreferredSize());
             table.setFillsViewportHeight(true);
 
-
-
             Dimension size = new Dimension(144,20);
-
+            //add reminder laber
             JLabel reminderLabel = new JLabel("Reminder: ");
             reminderLabel.setPreferredSize(new Dimension(133, 20));
             panel.add(reminderLabel);
+            //add comboxBox
             for (int i=0; i <=6; i++){
-//                //add label
-//                JLabel day = new JLabel(Constants.DAYS_OF_THE_WEEK[i]);
-//                day.setLocation((i+1)*150, 0);
-//                panel.add(day);
-
-                //add comboxBox
                 JComboBox<String> comboBox = new JComboBox<String>();
                 for (Task task : manager.getTimetable(term).getTaskCalendar().
                                 get(Constants.DAYS_OF_THE_WEEK[i]).toArray(new Task[0])) {
@@ -113,128 +103,20 @@ public class TimeTableScreen extends JFrame {
 
             panel.add(table);
             timeTableTabs.addTab(term, panel);
-
         }
-//        for (String day : table.getTaskCalendar().keySet()) {
-//            for (Task task : table.getTaskCalendar().get(day)) {
-//                switch (day) {
-//                    case "Monday":
-//                        comboBox1.addItem(task.getName());
-//                    case "Tuesday":
-//                        comboBox2.addItem(task.getName());
-//                    case "Wednesday":
-//                        comboBox3.addItem(task.getName());
-//                    case "Thursday":
-//                        comboBox4.addItem(task.getName());
-//                    case "Friday":
-//                        comboBox5.addItem(task.getName());
-//                    case "Saturday":
-//                        comboBox6.addItem(task.getName());
-//                    case "Sunday":
-//                        comboBox7.addItem(task.getName());
-//                }
-//            }
-//        }
     }
 
-////TODO needs window pop up when there is a conflict
+//TODO needs window pop up when there is a conflict
 
-//    private JTable createEmptyTable(){
-//        JTable jtable = new JTable();
-//        //set times
-//        for (int i=0; i<=23; i++) {
-//            String time = Constants.TIME[i];
-//            jtable.getModel().setValueAt(time, i+2, 0);
-//        }
-//        jtable.getModel().setValueAt("Reminder",1,0);
-//        for (int i=0;i<=6;i++){
-//            //set days
-//            String day = Constants.DAYS_OF_THE_WEEK[i];
-//            jtable.getModel().setValueAt(day, 0, i+1);
-//        }
-//
-//        //set the hour column width
-//        TableColumnModel columns = jtable.getColumnModel();
-//        columns.getColumn(0).setPreferredWidth(20);
-//
-//        //Center the text on all columns
-//        DefaultTableCellRenderer centreRenderer = new DefaultTableCellRenderer();
-//        centreRenderer.setHorizontalAlignment(JLabel.CENTER);
-//        for (int i = 0; i < 8; i ++) {
-//            columns.getColumn(i).setCellRenderer(centreRenderer);
-//        }
-//        return jtable;
-//    }
 
     //TODO extract method here
     private JTable fillTimeTable(TimeTable table) {
-////        for (int i=0;i<=6;i++) {
-////            //set reminders
-//        for (String day : table.getTaskCalendar().keySet()) {
-//
-//            ArrayList<String> tasks = new ArrayList<>();
-//            for (Task task : table.getTaskCalendar().get(day)) {
-//                tasks.add(task.getName());
-//            }
-//            System.out.println(tasks);
-//            String[] array = tasks.toArray(new String[0]);
-////            String[] strlist = new String[tasks.size()];
-////            for (int i = 0; i < tasks.size(); i++) {
-////                strlist[i] = tasks.get(i);
-////            }
-////            for (String s : strlist) {
-////                System.out.println(s);
-////                }
-//
-//                String[] newlst = new String[]{"sd"};
-//
-//                JComboBox<String> reminder = new JComboBox<String>(array);
-//                reminder.setVisible(true);
-//
-////                reminder.addActionListener(new ActionListener() {
-////                    public void actionPerformed(ActionEvent e) {
-////                        String selected = (String) reminder.getSelectedItem();
-////                        System.out.println(selected);
-////                    }
-////                });
-//                reminder.addItem("index 1");
-//                System.out.println(reminder.getItemAt(0));
-//
-//
-////                reminder.addItem(table.getTaskCalendar().get("Monday").get(0).getName());
-////                for (Task task : table.getTaskCalendar().get(day)) {
-////                    reminder.addItem(task.getName());
-////                }
-////                    for (int j = 0; j < table.getTaskCalendar().get(day).size(); j++) {
-////                        String name =  table.getTaskCalendar().get(day).get(j).getName();
-////                        reminder.addItem(name);
-////                        reminder.addItem("adsfa");
-////                    }
-////            reminder.addItem();
-//
-//                DefaultCellEditor defaultCellEditor = new DefaultCellEditor(reminder);
-//                editors.add(defaultCellEditor);
-//            }
-
-            JTable jtable = new JTable(defaultTableModel);
-//            {
-//                //  Determine editor to be used by row
-//                public TableCellEditor getCellEditor(int row, int column) {
-//                    int modelColumn = convertColumnIndexToModel(column);
-//
-//                    if (modelColumn >= 1 && row == 1)
-//                        return editors.get(row);
-//                    else
-//                        return super.getCellEditor(row, column);
-//                }
-//            };
-
+            JTable jtable = new JTable(25, 8);
             //set times
             for (int i = 0; i <= 23; i++) {
                 String time = Constants.TIME[i];
                 jtable.getModel().setValueAt(time, i+1, 0);
             }
-//            jtable.getModel().setValueAt("Reminder", 1, 0);
             for (int i = 0; i <= 6; i++) {
                 //set days
                 String day = Constants.DAYS_OF_THE_WEEK[i];
@@ -252,8 +134,6 @@ public class TimeTableScreen extends JFrame {
                 columns.getColumn(i).setCellRenderer(centreRenderer);
             }
 
-            //TODO set header with defaultTableModel?
-
             //schedule no task objects
             for (String day : table.getCalendar().keySet()) {
                 int columnIndex = Arrays.asList(Constants.DAYS_OF_THE_WEEK).indexOf(day) + 1;
@@ -261,7 +141,7 @@ public class TimeTableScreen extends JFrame {
                 for (int i = 0; i <= 23; i++) {
                     Events event = table.getCalendar().get(day)[i];
                     if (event != null) {
-                        int rowIndex = i + 2;
+                        int rowIndex = i + 1;
                         jtable.getModel().setValueAt(event, rowIndex, columnIndex);
                     }
                 }
