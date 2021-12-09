76b251d038d87d7495aa769a6dd6eb0862ae9d83
Aiden Lin <81498713+aiden-linhaoze@users.noreply.github.com>
2021-12-08 21:20:19 -0500

    Update Constants.java

--- src/Helpers/Constants.java
+++ src/Helpers/Constants.java
@@ -28,10 +28,9 @@ public class Constants {
     public static final String TRUE = "true";
     public static final String FALSE = "false";
     public static final String[] TIME =
-            {"00:00AM","01:00AM","02:00AM","03:00AM","04:00AM","05:00AM","06:00AM",
-                    "07:00AM", "08:00AM","09:00AM","10:00AM","11:00AM","12:00PM","01:00PM","02:00PM",
-                    "03:00PM","04:00PM","05:00PM","06:00PM","07:00PM", "08:00PM","09:00PM","10:00PM",
-                    "11:00PM"};
+            {"00:00AM","01:00AM","02:00AM","03:00AM","04:00AM","05:00AM","06:00AM","07:00AM",
+             "08:00AM","09:00AM","10:00AM","11:00AM","12:00PM","01:00PM","02:00PM","03:00PM",
+             "04:00PM","05:00PM","06:00PM","07:00PM","08:00PM","09:00PM","10:00PM","11:00PM"};
     public static final String SCHEDULE_COURSE = "Schedule Course";
     public static final String SCHEDULE_EVENT = "Schedule Event";
     public static final String REMOVE_EVENT = "Remove Event";
